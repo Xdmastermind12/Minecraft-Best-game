b5be75695a6c3463f41ea60cfa1cc0356f4d6b3d
Xdmastermind12 <77959541+Xdmastermind12@users.noreply.github.com>
2021-05-05 13:47:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -107,4 +107,40 @@ function player_update(){
             console.log("C");
           }
          }
-            
+            function up(){
+              if(player_y>=0){
+                player_y=player_y-block_image_height;
+                console.log("block image height= "+block_image_height);
+                console.log("when up arrow is clicked, X= "+player_x+",Y= "+player_y);
+                canvas.remove(player_object);
+                player_update();
+              }
+            }
+            function down(){
+              if(player_y<=500){
+                player_y=player_y+block_image_height;
+                console.log("block image height= "+block_image_height);
+                console.log("when down arrow is clicked, X= "+player_x+",Y= "+player_y);
+                canvas.remove(player_object);
+                player_update();
+              }
+            }
+            function left(){
+              if(player_x>=0){
+                player_x=player_x-block_image_width;
+                console.log("block image width= "+block_image_width);
+                console.log("when left arrow is clicked, X= "+player_x+",Y= "+player_y);
+                canvas.remove(player_object);
+                player_update();
+              }
+            }
+            function right(){
+              if(player_x<=850){
+                player_x=player_x+block_image_width;
+                console.log("block image width= "+block_image_width);
+                console.log("when right arrow is clicked, X= "+player_x+",Y= "+player_y);
+                canvas.remove(player_object);
+                player_update();
+              }
+            }
+         
